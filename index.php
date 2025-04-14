a295d1bb55d0f0a945581ea196e548404f3c9e0b
julia-daniluski <julia.o.daniluski@gmail.com>
2025-04-14 11:35:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
         </div>
         <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-12">
             <div class="card h-100">
                 <div class="card-header">
                         <h4 class="mb-0">
