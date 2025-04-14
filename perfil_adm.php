2e0b1c7eeb1812e4f11b780f38b5920d16944c93
julia-daniluski <julia.o.daniluski@gmail.com>
2025-04-14 09:25:05 -0300

    Update perfil_adm.php

--- perfil_adm.php
+++ perfil_adm.php
@@ -97,6 +97,50 @@
                 </div>
             </div>
         </div>
+
+        <!-- Tabela de veiculos cadastrados-->
+        <div class="row mt-4">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="mb-0">
+                            Veículos cadastrados
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <thead>
+                                    <th>Tipo</th>
+                                    <th>Modelos</th>
+                                    <th>Placa</th>
+                                    <th>Status</th>
+                                    <th>Ações</th>
+                                </thead>
+                                <tbody>
+
+                                    <tr>
+
+                                        <td>Carro</td>
+                                        <td>Uno</td>
+                                        <td>ABC1D23</td>
+                                        <td> <span class="badge bg-success">Disponivel</span></td>
+                                        <td>
+                                            <div class="action-wrapper">
+                                                
+                                            </div>
+                                        </td>
+
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
     
 </body>
